728db917489545300f2306385fa2080eea869d8b
Chris Dyer <redpony@gmail.com>
2010-05-07 08:55:12 -0400

    clean up extractor

--- extools/extractor.cc
+++ extools/extractor.cc
@@ -9,6 +9,7 @@
 #include <boost/program_options.hpp>
 #include <boost/program_options/variables_map.hpp>
 
+#include "sentence_pair.h"
 #include "tdict.h"
 #include "wordid.h"
 #include "array2d.h"
@@ -42,37 +43,6 @@ struct ParallelSpan {
   bool IsVariable() const { return i1 != -1; }
 };
 
-// represents a parallel sentence with a word alignment and category
-// annotations over subspans (currently in terms of f)
-struct AnnotatedParallelSentence {
-  vector<WordID> f, e;  // words in f and e
-  vector<int> e_aligned, f_aligned; // counts the number of times column/row x is aligned
-  Array2D<bool> aligned;
-
-  Array2D<vector<WordID> > span_types;  // span_types(i,j) is the list of category
-                               // types for a span (i,j) in the TARGET language.
-  int f_len, e_len;
-  vector<pair<int,int> > f_spans, e_spans;
-
-  // read annotated parallel sentence from string
-  void ParseInputLine(const char* buf);
-
- private:
-  void HandleAlignment(const char* buf, int start, int end);
-  void Reset() { f.clear(); e.clear(); e_aligned.clear(); f_aligned.clear(); aligned.clear(); span_types.clear(); f_spans.clear(); e_spans.clear(); }
-  void HandleSpan(const char* buf, int start, int end);
-
-  void AllocateForAlignment() {
-    aligned.resize(f.size(), e.size(), false);
-    f_aligned.resize(f.size(), 0);
-    e_aligned.resize(e.size(), 0);
-    span_types.resize(e.size(), e.size()+1);
-    f_len = f.size();
-    e_len = e.size();
-  }
-  static int ReadAlignmentPoint(const char* buf, int start, int end, bool permit_col, int* a, int* b);
-};
-
 void InitCommandLine(int argc, char** argv, po::variables_map* conf) {
   po::options_description opts("Configuration options");
   opts.add_options()
@@ -102,130 +72,6 @@ void InitCommandLine(int argc, char** argv, po::variables_map* conf) {
   }
 }
 
-inline bool IsWhitespace(char c) { return c == ' ' || c == '\t'; }
-
-inline void SkipWhitespace(const char* buf, int* ptr) {
-  while (buf[*ptr] && IsWhitespace(buf[*ptr])) { ++(*ptr); }
-}
-
-int AnnotatedParallelSentence::ReadAlignmentPoint(const char* buf, int start, int end, bool permit_col, int* a, int* b) {
-  if (end - start < 3) {
-    cerr << "Alignment point badly formed: " << string(buf, start, end-start) << endl; abort();
-  }
-  int c = start;
-  *a = 0;
-  while(c < end && buf[c] != '-') {
-    if (buf[c] < '0' || buf[c] > '9') {
-      cerr << "Alignment point badly formed: " << string(buf, start, end-start) << endl;
-      abort();
-    }
-    (*a) *= 10;
-    (*a) += buf[c] - '0';
-    ++c;
-  }
-  ++c;
-  if (c >= end) {
-    cerr << "Alignment point badly formed: " << string(buf, start, end-start) << endl; abort();
-  }
-  (*b) = 0;
-  while(c < end && (!permit_col || (permit_col && buf[c] != ':'))) {
-    if (buf[c] < '0' || buf[c] > '9') {
-      cerr << "Alignment point badly formed: " << string(buf, start, end-start) << endl;
-      abort();
-    }
-    (*b) *= 10;
-    (*b) += buf[c] - '0';
-    ++c;
-  }
-  return c;
-}
-
-void AnnotatedParallelSentence::HandleAlignment(const char* buf, int start, int end) {
-  int a, b;
-  ReadAlignmentPoint(buf, start, end, false, &a, &b);
-  assert(a < f_len);
-  assert(b < e_len);
-  aligned(a,b) = true;
-  ++f_aligned[a];
-  ++e_aligned[b];
-  // cerr << a << " " << b << endl;
-}
-
-void AnnotatedParallelSentence::HandleSpan(const char* buf, int start, int end) {
-  int a,b;
-  int c = ReadAlignmentPoint(buf, start, end, true, &a, &b) + 1;
-  if (buf[c-1] != ':' || c >= end) {
-    cerr << "Span badly formed: " << string(buf, start, end-start) << endl; abort();
-  }
-  // cerr << a << " " << b << " " << string(buf,c,end-c) << endl;
-  span_types(a,b).push_back(-TD::Convert(string(buf, c, end-c)));
-}
-
-// INPUT FORMAT
-// ein haus ||| a house ||| 0-0 1-1 ||| 0-0:DT 1-1:NN 0-1:NP
-void AnnotatedParallelSentence::ParseInputLine(const char* buf) {
-  Reset();
-  int ptr = 0;
-  SkipWhitespace(buf, &ptr);
-  int start = ptr;
-  int state = 0;  // 0 = French, 1 = English, 2 = Alignment, 3 = Spans
-  while(char c = buf[ptr]) {
-    if (!IsWhitespace(c)) { ++ptr; continue; } else {
-      if (ptr - start == 3 && buf[start] == '|' && buf[start+1] == '|' && buf[start+2] == '|') {
-        ++state;
-        if (state == 4) { cerr << "Too many fields (ignoring):\n  " << buf << endl; return; }
-        if (state == 2) {
-          // cerr << "FLEN=" << f->size() << " ELEN=" << e->size() << endl;
-          AllocateForAlignment();
-        }
-        SkipWhitespace(buf, &ptr);
-        start = ptr;
-        continue;
-      }
-      switch (state) {
-        case 0:  f.push_back(TD::Convert(string(buf, start, ptr-start))); break;
-        case 1:  e.push_back(TD::Convert(string(buf, start, ptr-start))); break;
-        case 2:  HandleAlignment(buf, start, ptr); break;
-        case 3:  HandleSpan(buf, start, ptr); break;
-        default: cerr << "Can't happen\n"; abort();
-      }
-      SkipWhitespace(buf, &ptr);
-      start = ptr;
-    }
-  }
-  if (ptr > start) {
-    switch (state) {
-      case 0:  f.push_back(TD::Convert(string(buf, start, ptr-start))); break;
-      case 1:  e.push_back(TD::Convert(string(buf, start, ptr-start))); break;
-      case 2:  HandleAlignment(buf, start, ptr); break;
-      case 3:  HandleSpan(buf, start, ptr); break;
-      default: cerr << "Can't happen\n"; abort();
-    }
-  }
-  if (state < 2) {
-    cerr << "Not enough fields: " << buf << endl;
-    abort();
-  }
-  if (e.empty() || f.empty()) {
-    cerr << "Sentences must not be empty: " << buf << endl;
-  }
-
-  // for each alignment point in e, precompute the minimal consistent phrases in f
-  // for each alignment point in f, precompute the minimal consistent phrases in e
-  e_spans.resize(e_len, pair<int,int>(f_len, 0));
-  f_spans.resize(f_len, pair<int,int>(e_len, 0));
-  for (int i = 0; i < f_len; ++i) {
-    for (int j = 0; j < e_len; ++j) {
-      if (aligned(i,j)) {
-        if (j < f_spans[i].first) f_spans[i].first = j;
-        f_spans[i].second = j+1;
-        if (i < e_spans[j].first) e_spans[j].first = i;
-        e_spans[j].second = i+1;
-      }
-    }
-  }
-}
-
 void LoosenPhraseBounds(const AnnotatedParallelSentence& sentence,
                         vector<ParallelSpan>* phrases) {
   assert(!"not implemented - TODO");
@@ -237,6 +83,22 @@ void ExtractBasePhrases(const int max_base_phrase_size,
                         const AnnotatedParallelSentence& sentence,
                         vector<ParallelSpan>* phrases) {
   phrases->clear();
+
+  vector<pair<int,int> > f_spans(sentence.f_len, pair<int,int>(sentence.e_len, 0));
+  vector<pair<int,int> > e_spans(sentence.e_len, pair<int,int>(sentence.f_len, 0));
+  // for each alignment point in e, precompute the minimal consistent phrases in f
+  // for each alignment point in f, precompute the minimal consistent phrases in e
+  for (int i = 0; i < sentence.f_len; ++i) {
+    for (int j = 0; j < sentence.e_len; ++j) {
+      if (sentence.aligned(i,j)) {
+        if (j < f_spans[i].first) f_spans[i].first = j;
+        f_spans[i].second = j+1;
+        if (i < e_spans[j].first) e_spans[j].first = i;
+        e_spans[j].second = i+1;
+      }
+    }
+  }
+
   for (int i1 = 0; i1 < sentence.f_len; ++i1) {
     if (sentence.f_aligned[i1] == 0) continue;
     int j1 = sentence.e_len;
@@ -245,14 +107,14 @@ void ExtractBasePhrases(const int max_base_phrase_size,
     for (int i2 = i1 + 1; i2 <= i_limit; ++i2) {
       if (sentence.f_aligned[i2-1] == 0) continue;
       // cerr << "F has aligned span " << i1 << " to " << i2 << endl;
-      j1 = min(j1, sentence.f_spans[i2-1].first);
-      j2 = max(j2, sentence.f_spans[i2-1].second);
+      j1 = min(j1, f_spans[i2-1].first);
+      j2 = max(j2, f_spans[i2-1].second);
       if (j1 >= j2) continue;
       if (j2 - j1 > max_base_phrase_size) continue;
       int condition = 0;
       for (int j = j1; j < j2; ++j) {
-        if (sentence.e_spans[j].first < i1) { condition = 1; break; }
-        if (sentence.e_spans[j].second > i2) { condition = 2; break; }
+        if (e_spans[j].first < i1) { condition = 1; break; }
+        if (e_spans[j].second > i2) { condition = 2; break; }
       }
       if (condition == 1) break;
       if (condition == 2) continue;

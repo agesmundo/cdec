fbe12f0cc09e87f8bcf9697cb64fd1ea89518164
andrea gesmundo <andrea.gesmundo@gmail.com>
2011-06-03 14:18:29 +0200

    adding Undirected rescorer structure

--- decoder/apply_models.cc
+++ decoder/apply_models.cc
@@ -25,8 +25,96 @@ typedef SmallVectorInt JVector;
 typedef vector<Candidate*> CandidateHeap;
 typedef vector<Candidate*> CandidateList;
 
+struct UCandidate;
+typedef vector<UCandidate*> UCandidateHeap;
+typedef vector<UCandidate*> UCandidateList;
+
 // default vector size (* sizeof string is memory used)
 static const size_t kRESERVE_NUM_NODES = 500000ul;
+
+//Undirected Candidate
+struct UCandidate {
+  int node_index_;                     // -1 until incorporated
+                                       // into the +LM forest
+  const Hypergraph::Edge* in_edge_;    // in -LM forest
+  //Hypergraph::Edge out_edge_;				 //out-hg is assembled at the end of decoding
+  //FFState state_;
+  //const JVector j_; //not needed in GU since should store ids in D[node][]
+  prob_t vit_prob_;            // these are fixed until the cand
+                               // is popped, then they may be updated
+  prob_t est_prob_;
+
+  UCandidate(const Hypergraph::Edge& e,
+            //const JVector& j,
+            const Hypergraph& out_hg,
+            const vector<UCandidateList>& D,
+            const FFStates& node_states,
+            const SentenceMetadata& smeta,
+            const ModelSet& models,
+            bool is_goal) :
+      node_index_(-1),
+      in_edge_(&e)//,
+      //j_(j)
+  {
+    InitializeUCandidate(out_hg, smeta, D, node_states, models, is_goal);
+  }
+
+  // used to query uniqueness
+  UCandidate(const Hypergraph::Edge& e/*,const JVector& j*/) : in_edge_(&e)/*, j_(j)*/ {}
+
+  bool IsIncorporatedIntoHypergraph() const {
+    return node_index_ >= 0;
+  }
+
+  void InitializeUCandidate(const Hypergraph& out_hg,
+                           const SentenceMetadata& smeta,
+                           const vector<vector<UCandidate*> >& D,
+                           const FFStates& node_states,
+                           const ModelSet& models,
+                           const bool is_goal) {
+//    const Hypergraph::Edge& in_edge = *in_edge_;
+//    out_edge_.rule_ = in_edge.rule_;
+//    out_edge_.feature_values_ = in_edge.feature_values_;
+//    out_edge_.i_ = in_edge.i_;
+//    out_edge_.j_ = in_edge.j_;
+//    out_edge_.prev_i_ = in_edge.prev_i_;
+//    out_edge_.prev_j_ = in_edge.prev_j_;
+//    Hypergraph::TailNodeVector& tail = out_edge_.tail_nodes_;
+//    tail.resize(j_.size());
+//    prob_t p = prob_t::One();
+    // cerr << "\nEstimating application of " << in_edge.rule_->AsString() << endl;
+//    for (int i = 0; i < tail.size(); ++i) {
+//      const UCandidate& ant = *D[in_edge.tail_nodes_[i]][j_[i]];
+//      assert(ant.IsIncorporatedIntoHypergraph());
+//      tail[i] = ant.node_index_;
+//      p *= ant.vit_prob_;
+//    }
+//    prob_t edge_estimate = prob_t::One();
+//    if (is_goal) {
+//      assert(tail.size() == 1);
+//      const FFState& ant_state = node_states[tail.front()];
+//      models.AddFinalFeatures(ant_state, &out_edge_, smeta);
+//    } else {
+//      models.AddFeaturesToEdge(smeta, out_hg, node_states, &out_edge_, &state_, &edge_estimate);
+//    }
+//    vit_prob_ = out_edge_.edge_prob_ * p;
+//    est_prob_ = vit_prob_ * edge_estimate;
+  }
+};
+
+//ostream& operator<<(ostream& os, const UCandidate& cand) {
+//  os << "CAND[";
+//  if (!cand.IsIncorporatedIntoHypergraph()) { os << "PENDING "; }
+//  else { os << "+LM_node=" << cand.node_index_; }
+//  os << " edge=" << cand.in_edge_->id_;
+//  os << " j=<";
+//  for (int i = 0; i < cand.j_.size(); ++i)
+//    os << (i==0 ? "" : " ") << cand.j_[i];
+//  os << "> vit=" << log(cand.vit_prob_);
+//  os << " est=" << log(cand.est_prob_);
+//  return os << ']';
+//}
+
 
 // life cycle: candidates are created, placed on the heap
 // and retrieved by their estimated cost, when they're
@@ -125,10 +213,16 @@ struct HeapCandCompare {
   bool operator()(const Candidate* l, const Candidate* r) const {
     return l->est_prob_ < r->est_prob_;
   }
+  bool operator()(const UCandidate* l, const UCandidate* r) const {
+    return l->est_prob_ < r->est_prob_;
+  }
 };
 
 struct EstProbSorter {
   bool operator()(const Candidate* l, const Candidate* r) const {
+    return l->est_prob_ > r->est_prob_;
+  }
+  bool operator()(const UCandidate* l, const UCandidate* r) const {
     return l->est_prob_ > r->est_prob_;
   }
 };
@@ -183,6 +277,7 @@ public:
     int every = 1;
     if (num_nodes > 100) every = 10;
     assert(in.nodes_[pregoal].out_edges_.size() == 1);
+    UCandidateHeap cands; //unique queue/heap of candidates
     if (!SILENT) cerr << "    ";
     for (int i = 0; i < in.nodes_.size(); ++i) {
       if (!SILENT && i % every == 0) cerr << '.';
